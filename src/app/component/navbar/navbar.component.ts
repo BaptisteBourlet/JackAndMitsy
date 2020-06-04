3d4fd54f8b16d01e2a474a340469d34ea12c261a
BaptisteBourlet <baptiste.bourlet@gmail.com>
2020-06-04 15:09:15 +0200

    debug

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,52 +1,55 @@
 import { Component, OnInit, HostListener, Inject } from '@angular/core';
-import { trigger, state, transition, style, animate } from '@angular/animations';  
+import { trigger, state, transition, style, animate } from '@angular/animations';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  animations:[ 
+  animations: [
     trigger('fade',
-    [ 
-      state('void', style({ opacity : 0})),
-      transition(':enter',[ animate(300)]),
-      transition(':leave',[ animate(500)]),
-    ]
-)]
+      [
+        state('void', style({ opacity: 0 })),
+        transition(':enter', [animate(300)]),
+        transition(':leave', [animate(500)]),
+      ]
+    )]
 })
 export class NavbarComponent implements OnInit {
 
   constructor(@Inject(DOCUMENT) document) { }
 
- 
+
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(e) {
-     if (window.pageYOffset > 400) {
-       let element = document.getElementById('navbar');
-       element.classList.add('sticky');
-     } else {
+    if (window.pageYOffset > 400) {
       let element = document.getElementById('navbar');
-        element.classList.remove('sticky'); 
-     }
+      element.classList.add('sticky');
+    } else {
+      let element = document.getElementById('navbar');
+      element.classList.remove('sticky');
+    }
   }
   isShow
   ngOnInit() {
-    if (screen.width < 800){
+    if (screen.width < 800) {
       this.isShow = true;
     }
   }
- 
 
-  
+
+
   icon = 'assets/images/show.svg';
   toggleDisplay() {
-    
+
     this.isShow = !this.isShow;
     if (this.isShow === false) {
       this.icon = 'assets/images/hide.svg';
-  } else {
+    } else {
       this.icon = 'assets/images/show.svg'
+    }
   }
+  hideNav() {
+    this.isShow = true
   }
 }
